8fad113f2ecf149f7246d8ab69da32e860144974
maxtq <maxtquaglia@gmail.com>
2026-04-06 10:44:56 -0300

    Revert "Merge branch 'max'"
    This reverts commit 298205df55c4b08c92e859689f6674ec0557d32e, reversing
    changes made to d22410ec2397c2acae74f198e2ef78a6c2900fb3.

--- Projeto Base/Assets/Scripts/Platform.cs
+++ Projeto Base/Assets/Scripts/Platform.cs
@@ -2,23 +2,22 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    public Transform alvo;
-    public float velocidade = 15f;
-
-    public Vector3 DeltaMovimento { get; private set; }
-
-    private Vector3 ultimaPosicao;
-
-    void Start()
-    {
-        ultimaPosicao = transform.position;
-    }
-
+    public Transform alvo;      // Objeto que será o centro da órbita
+    public float velocidade = 15f; // Velocidade da rotação
     void Update()
     {
         transform.RotateAround(alvo.position, Vector3.up, velocidade * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Platform"))
+            this.transform.parent = collision.transform;
+    }
 
-        DeltaMovimento = transform.position - ultimaPosicao;
-        ultimaPosicao = transform.position;
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Platform"))
+            this.transform.parent = null;
+        
     }
 }
