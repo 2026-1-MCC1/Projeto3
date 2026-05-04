4cd61e21283f35bdabad53b2fa4c8b00e908b32d
RafaelDominguesBoccia <rd.boccia@gmail.com>
2026-05-04 07:27:34 -0300

    Organização das dos projetos na pasta source e melhora no sistema de som

---
+++ src/Projeto Base/Assets/Scripts/SceneConfigs.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class SceneConfigs : MonoBehaviour
+{
+    public static float globalVolume = 100f;
+    public static float musicVolume = 100f;
+    public static float effectsVolume = 100f;
+}
