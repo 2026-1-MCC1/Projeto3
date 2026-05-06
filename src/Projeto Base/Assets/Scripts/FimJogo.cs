8e9c719f70eac85af11367be2570ca334ba6bc99
Victor Oliveira <victoroli359@gmail.com>
2026-05-06 11:47:11 -0300

    tela de derrota
    tela de derrota

---
+++ src/Projeto Base/Assets/Scripts/FimJogo.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FimJogo : MonoBehaviour
+{
+    public void Exibir()
+    {
+        this.gameObject.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0;
+    }
+
+    public void Esconder()
+    {
+        this.gameObject.SetActive(false);
+    }
+    public void TentarNovamente()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Movimentação personagem versão 3");
+
+    }
+    public void SairJogo()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
